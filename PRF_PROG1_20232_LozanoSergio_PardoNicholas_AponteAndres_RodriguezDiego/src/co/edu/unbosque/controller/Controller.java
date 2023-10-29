cade8e10b2bea921b6cb3eeee874c5b2d0aef678
sergi <sergi@Sergio_Lozano>
2023-10-29 00:40:28 -0500

    Implementacion base del controlador

--- PRF_PROG1_20232_LozanoSergio_PardoNicholas_AponteAndres_RodriguezDiego/src/co/edu/unbosque/controller/Controller.java
+++ PRF_PROG1_20232_LozanoSergio_PardoNicholas_AponteAndres_RodriguezDiego/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,82 @@
 package co.edu.unbosque.controller;
 
-public class Controller {
+import co.edu.unbosque.model.*;
+import co.edu.unbosque.view.*;
 
-}
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+
+
+public class Controller implements ActionListener{
+	 
+	  private View vista;
+	  private Model modelo;
+
+		
+	  
+  public Controller(){
+	  
+	  vista= new View();   
+	  modelo= new Model();
+
+	  add_components();  
+	  ejecutar();
+  }
+	  
+	  
+
+  public void ejecutar() {
+		
+
+  }
+  
+  
+  
+  public void add_components() {           
+	  
+/*	 
+	  
+	 vista.getPanelInicio().getJugador().addActionListener(this);
+	 vista.getPanelInicio().getJugador().setActionCommand("jugador");
+	 
+	 vista.getPanelJugadores().getButton().addActionListener(this);
+	 vista.getPanelJugadores().getButton().setActionCommand("validar");
+*/	 
+	 
+
+   }
+
+  
+  public void Eventos(ActionEvent Eventos_Inicio) {
+		
+		
+		switch(Eventos_Inicio.getActionCommand()) {
+		
+		case "jugador":
+		
+                
+		break;
+		
+		
+		
+	
+		
+		}
+		
+  }
+  
+  
+//-----------------------------------------------------------------------------------------------------------------------------  
+  
+  @Override
+  public void actionPerformed(ActionEvent e) {
+	  
+	// Activacion de los eventos 
+               Eventos(e);
+  	
+  }
+	
+ }
